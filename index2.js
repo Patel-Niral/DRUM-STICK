2f5f3d26944b50999cd27d5b71a313270d25644c
Patel-Niral <144341032+Patel-Niral@users.noreply.github.com>
2024-06-04 13:18:39 +0530

    sound function created

--- index2.js
+++ index2.js
@@ -1,57 +1,44 @@
-const numofdrum = document.querySelectorAll(".drum").length;
-alert(numofdrum + " drum are present");
-
-
+let numofdrum = document.querySelectorAll(".drum").length;
 
 function handleclick() {
-    const drumele = this.innerHTML;
+    let buttonInnerHTML = this.innerHTML;
+    makeSound(buttonInnerHTML);
+}
 
-    
-  switch(drumele){
+function makeSound(key) {
+    switch (key) {
+        case "w":
+            var audio = new Audio("sounds/crash.mp3");
+            audio.play();
+            break;
+        case "a":
+            var audio = new Audio("sounds/kick-bass.mp3");
+            audio.play();
+            break;
+        case "s":
+            var audio = new Audio("sounds/snare.mp3");
+            audio.play();
+            break;
+        case "d":
+            var audio = new Audio("sounds/tom-1.mp3");
+            audio.play();
+            break;
+        case "j":
+            var audio = new Audio("sounds/tom-2.mp3");
+            audio.play();
+            break;
+        case "k":
+            var audio = new Audio("sounds/tom-3.mp3");
+            audio.play();
+            break;
+        case "l":
+            var audio = new Audio("sounds/tom-4.mp3");
+            audio.play();
+            break;
+        default: console.log(key);
+    }
+}
 
-         case "w":
-        let crash = new Audio('sounds/crash.mp3');
-        crash.play();
-        break;
-        
-         case "a":
-        let  kick = new Audio('sounds/kick-bass.mp3');
-        kick.play();
-        break;
-
-    case "s":   
-        let snare = new Audio('sounds/snare.mp3');
-        snare.play();
-        break;
-
-    case "d":   
-        let tom1 = new Audio('sounds/tom-1.mp3');
-        tom1.play();
-        break;
-
-    case "j":   
-        let tom2 = new Audio('sounds/tom-2.mp3');
-        tom2.play();
-        break;
-
-    case "k":   
-        let tom3 = new Audio('sounds/tom-3.mp3');
-        tom3.play();
-        break;
-    case "l":   
-        let tom4 = new Audio('sounds/tom-4.mp3');
-        tom4.play();
-        break;
-
-        default:
-            console.log("drum not present")
-
-  }
-
-  }
-
-
-for (const i = 0; i < numofdrum; i++) {
-    document.querySelectorAll(".drum")[i].addEventListener("click", handleclick);
-
+for (let i = 0; i < numofdrum; i++) {
+    document.querySelectorAll(".drum")[i].addEventListener("click", handleclick);
 }
