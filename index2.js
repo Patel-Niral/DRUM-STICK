8f2ab95806e25703053b4481acd0f036a1f4ecb3
Patel-Niral <144341032+Patel-Niral@users.noreply.github.com>
2024-06-05 11:52:05 +0530

    keyboard eventlistener added

--- index2.js
+++ index2.js
@@ -1,43 +1,58 @@
 let numofdrum = document.querySelectorAll(".drum").length;
 
+
+
+// mouse click handling
 function handleclick() {
     let buttonInnerHTML = this.innerHTML;
     makeSound(buttonInnerHTML);
-}
+    document.addEventListener(keypress, makeSound(key.event))
 
-function makeSound(key) {
-    switch (key) {
-        case "w":
-            var audio = new Audio("sounds/crash.mp3");
-            audio.play();
-            break;
-        case "a":
-            var audio = new Audio("sounds/kick-bass.mp3");
-            audio.play();
-            break;
-        case "s":
-            var audio = new Audio("sounds/snare.mp3");
-            audio.play();
-            break;
-        case "d":
-            var audio = new Audio("sounds/tom-1.mp3");
-            audio.play();
-            break;
-        case "j":
-            var audio = new Audio("sounds/tom-2.mp3");
-            audio.play();
-            break;
-        case "k":
-            var audio = new Audio("sounds/tom-3.mp3");
-            audio.play();
-            break;
-        case "l":
-            var audio = new Audio("sounds/tom-4.mp3");
-            audio.play();
-            break;
-        default: console.log(key);
+
+    function makeSound(key) {
+        switch (key) {
+            case "w":
+                var audio = new Audio("sounds/crash.mp3");
+                audio.play();
+                break;
+            case "a":
+                var audio = new Audio("sounds/kick-bass.mp3");
+                audio.play();
+                break;
+            case "s":
+                var audio = new Audio("sounds/snare.mp3");
+                audio.play();
+                break;
+            case "d":
+                var audio = new Audio("sounds/tom-1.mp3");
+                audio.play();
+                break;
+            case "j":
+                var audio = new Audio("sounds/tom-2.mp3");
+                audio.play();
+                break;
+            case "k":
+                var audio = new Audio("sounds/tom-3.mp3");
+                audio.play();
+                break;
+            case "l":
+                var audio = new Audio("sounds/tom-4.mp3");
+                audio.play();
+                break;
+            default: console.log(key);
+        }
     }
 }
+
+
+
+// keyboard event handling
+
+// document.addEventListener(keypress, makeSound(key.event))
+
+
+
+
 
 for (let i = 0; i < numofdrum; i++) {
     document.querySelectorAll(".drum")[i].addEventListener("click", handleclick);
